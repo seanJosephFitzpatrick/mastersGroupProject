79ad7e70cf7c8aa83736be2cbc3c04b07da6f74c
A00248213 <A00248213@W301-A16.studentnet.ait.ie>
2018-02-08 16:50:57 +0000

    deleted un-used classes and created Mcc and Mnc related classes

---
+++ src/main/java/com/mase2/mase2_project/model/MccMnc.java
@@ -0,0 +1,51 @@
+package com.mase2.mase2_project.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the mcc_mnc database table.
+ * 
+ */
+@Entity
+@Table(name="mcc_mnc")
+@NamedQuery(name="MccMnc.findAll", query="SELECT m FROM MccMnc m")
+public class MccMnc implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private MccMncPK id;
+
+	private String country;
+
+	private String operator;
+
+	public MccMnc() {
+	}
+
+	public MccMncPK getId() {
+		return this.id;
+	}
+
+	public void setId(MccMncPK id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getOperator() {
+		return this.operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+}
