7e146f529416da2cec009b7c0fa318297a461ea4
unknown <A00248115@W301-A18.studentnet.ait.ie>
2018-02-15 15:09:36 +0000

    removing unused imports/ testing jenkins bitbucket-hook

--- src/main/java/com/mase2/mase2_project/rest/BaseDataEndpoint.java
+++ src/main/java/com/mase2/mase2_project/rest/BaseDataEndpoint.java
@@ -7,15 +7,12 @@ import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
