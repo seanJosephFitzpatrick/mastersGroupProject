a938816615d6c9536a79d55052d5fda102cc9e6e
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2018-03-15 18:14:57 +0000

    Commit query changes

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -4,6 +4,7 @@ $('document').ready(function() {
 	$('#wrapper').css({backgroundColor:"white"});
 	sessionStorage.setItem("role", "none");
 	showManiPage();
+	$('[data-toggle="modal"]').tooltip()
 });
 
 function showLoading(){
