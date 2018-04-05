d4febd8583f553814c5314183b1be02d3950e30e
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2018-04-05 22:50:57 +0100

    Add modelGraph.js

---
+++ src/main/webapp/resources/js/modelGraph.js
@@ -0,0 +1,49 @@
+$(function() {
+  $.getJSON("", function(data) {
+    console.log(data);
+    Highcharts.chart('container', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Model Codes'
+      },
+      subtitle: {
+        text: 'Unique Cause Code and Event Id Failures'
+      },
+      xAxis: {
+        type: 'category',
+        categories: data.map(function(x) {
+          return x.eventCause.description;
+        })
+      },
+      yAxis: {
+        title: {
+          text: 'Number of Occurrences'
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          borderWidth: 0,
+          dataLabels: {
+            enabled: true,
+            format: '{point.y:.0f}'
+          }
+        }
+      },
+      tooltip: {
+        //headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b> Occurrences<br/>'
+      },
+      series: [{
+        colorByPoint: true,
+        data: data.map(function(x) {
+          return x.count * 1;
+        })
+		}],
+    });
+  });
+});
